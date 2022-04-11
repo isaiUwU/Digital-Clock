8c6b4f06422ed0c5bc7bd498b5eb4aface8bb9d2
Isaiw <78402589+isaiUwU@users.noreply.github.com>
2022-04-11 14:06:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calculateTime(){
     hour = hour % 12;
     hour = hour ? hour : '12';
     hour = hour < 10 ? '0' + hour : hour;
-    minute = minute < 10 ? '0'.minute : minute;
+    minute = minute < 10 ? '0.'minute : minute;
 
     document.getElementById("day").innerHTML = dayNames[dayNumber];
     document.getElementById("hour").innerHTML = 
